89369233b1ea94b4dff7cfbf794a067cf5727c1d
Sarah <110872790+swangpan@users.noreply.github.com>
2022-09-15 09:06:35 -0500

    Fruity vice json data visualization
    normalized fruityvice json data and put it into a table format

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,3 +27,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# Making the json data and normalizing it 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# Displaying data in a table 
+streamlit.dataframe(fruityvice_normalized)
