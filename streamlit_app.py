9d492e3814e213a955095c3bf532d24852b4b1e6
Sarah <110872790+swangpan@users.noreply.github.com>
2022-09-19 13:16:51 -0500

    Added function and button
    get_fruit_load_list function

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,6 @@ def get_fruityvice_data(this_fruit_choice):
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     return fruityvice_normalized
   
-  
 #New Section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 try:
@@ -44,7 +43,20 @@ try:
 
 except URLError as e:
   streamlit.error()
-  
+ 
+streamlit.header("The fruit load list contains:")
+#Snowflake-related functions
+def get_fruit_load_list():
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("select * from fruit_load_list")
+        return my_cur.fetchall()
+
+# Add a button to load the fruit
+if steamlit.button('Get Fruit Load List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_rows = get_fruit_load_list()
+    streamlit.dataframe(my_data_rows)
+        
 #don't run anything past here while we troubleshoot
 streamlit.stop()
 
