c082b570ce3dc82b5713dca72526e6c297a10bf4
Sarah <110872790+swangpan@users.noreply.github.com>
2022-09-19 13:38:07 -0500

    Deleted unused code

--- streamlit_app.py
+++ streamlit_app.py
@@ -71,22 +71,4 @@ if streamlit.button('Add a Fruit to the List'):
     streamlit.text(back_from_function)
     my_cnx.close()
     
-        
-#don't run anything past here while we troubleshoot
-streamlit.stop()
-
-#import snowflake.connector
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
-
-# Allow the end user to add a fruit to the list
-add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
-streamlit.write("Thanks for adding " + add_my_fruit)
-
-#This will not work correctly, but just go with it for now
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+ 
