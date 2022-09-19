ddadebd99246cef1b3bcdd8dfb5c3ffe03a85b46
Sarah <110872790+swangpan@users.noreply.github.com>
2022-09-19 11:26:55 -0500

    Allow user to pick fruit again

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,3 +40,7 @@ my_cur.execute("select * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
+
+# Allow the end user to add a fruit to the list
+add_my_fruit = streamlit.multiselect("What fruit would you like to add?", list(my_data_rows.index),['jackfruit'])
+fruits_to_show = my_fruit_list.loc[add_my_fruit]
